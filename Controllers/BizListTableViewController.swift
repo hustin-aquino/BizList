9b1555a8a55af5a2497e1ad002efe201b1940911
hustin-aquino <justinmosesbitancor@gmail.com>
2024-09-27 22:51:30 +0800

    Adding BizListTableViewController and TaskListViewController and their respective UI on Main.Storyboard

--- Controllers/BizListTableViewController.swift
+++ Controllers/BizListTableViewController.swift
@@ -1,19 +1,17 @@
 //
-//  ViewController.swift
+//  BizListTableViewController.swift
 //  BizList
 //
 //  Created by justin.bitancor on 9/27/24.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-
+class BizListTableViewController: UITableViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
 }
-
---
+++ Controllers/TaskListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TaskListViewController.swift
+//  BizList
+//
+//  Created by justin.bitancor on 9/27/24.
+//
+
+import Foundation
+import UIKit
+
+class TaskListViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
